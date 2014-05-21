e9576473ba50342ba9a8cc764dccdda15ca62e34
zhang.mingqiang <zhang.mingqiang@immomo.com>
2014-05-21 12:06:59 +0800

    finish demo version

--- resp.go
+++ resp.go
@@ -1,63 +1,77 @@
 package resp
 
 import (
-	"io/ioutil"
+	"io"
+	"errors"
+	"bytes"
+	"strconv"
 )
 
 const (
-	var T_SimpleString = '+'
-	var T_Errors	   = '-'
-	var T_Integers	   = ':'
-	var T_BulkStrings  = '$'
-	var T_Arrays	   = '*'
+	T_SimpleString = '+'
+	T_Error	   = '-'
+	T_Integer	   = ':'
+	T_BulkString  = '$'
+	T_Array	   = '*'
 )
 
 type Data struct {
 	T byte
 	str []byte
 	num int64
-	arrays []Data
+	array []*Data
 
 }
 
 //string\bulkString
-func (d Data) String() []byte {
-
+func (d *Data) String() string {
+	return string(d.str)
 }
 
-func (d Data) Errors() string {
-
+func (d *Data) Byte() []byte {
+	return d.str
 }
 
-func (d Data) Array() []Data {
-
+func (d *Data) Error() string {
+	return string(d.str)
 }
 
-func ReadData(r io.Reader) (Data, error) {
+func (d *Data) Integer() int64 {
+	return d.num
+}
+
+func (d *Data) Array() []*Data {
+	return d.array
+}
+
+func ReadData(r io.Reader) (*Data, error) {
 
 	var buf []byte
 	var n int
 	var err error
 
-	var requestType []byte
-	requestType = make([]byte, 1)
-	n, err = io.ReadAtLeast(r, buf, 1)
+	buf = make([]byte, 1)
+	n, err = io.ReadFull(r, buf)
 	if nil != err {
-		return errors.New("err_first_byte")
+		return nil, errors.New("err_first_byte")
 	}
 
-	ret := Data{}
+	if n==1 {
+	//
+	}
+
+	ret := &Data{}
 	switch buf[0] {
 		case '+':
-			ret.T = T_SimgpleString
-			ret.common, err = readRespLine(r)
+			ret.T = T_SimpleString
+			ret.str, err = readRespLine(r)
 
 		case '-':
-			ret.T = T_Errors
-			ret.common, err = readRespLine(r)
+			ret.T = T_Error
+			ret.str, err = readRespLine(r)
 
 		case ':':
-			ret.T = T_Integers
+			ret.T = T_Integer
 			ret.num, err = readRespIntLine(r)
 
 		case '$':
@@ -65,31 +79,77 @@ func ReadData(r io.Reader) (Data, error) {
 			lenBulkString, err = readRespIntLine(r)
 
 			ret.T = T_BulkString
-			ret.common, err = readRespN(r, lenBulkString)
+			ret.str, err = readRespN(r, lenBulkString)
+
+			//read the followed \r\n
+			_, err = readRespN(r, 2)
 
 		case '*':
 			var lenArray int64
+			var i int64
 			lenArray, err = readRespIntLine(r)
-			data.array = make(Data, lenArray)
-			for i:=0; i<lenArray; i++ {
-				data.array[i], err = readData(r)
+
+			ret.T = T_Array
+			if nil==err {
+				ret.array = make([]*Data, lenArray)
+				for i=0; i<lenArray; i++ {
+					ret.array[i], err = ReadData(r)
+				}
 			}
 
 	}
-	return data, err
+	return ret, err
 }
 
 //读取当前行，并去掉最后的\r\n
-func readRespLine(r io.Reader, d byte) ([]byte, error) {
+func readRespLine(r io.Reader) ([]byte, error) {
 
+	var n, i int
+	var err error
+	var buf []byte
+	var ret *bytes.Buffer
+
+	buf = make([]byte, 1)
+	ret = &bytes.Buffer{}
+
+	for {
+		n, err = io.ReadFull(r, buf)
+		if nil != err {
+			return nil, err
+		}
+
+		if n==0 {
+			continue
+		}
+
+		i++
+		ret.WriteByte(buf[0])
+		if '\n' == buf[0] {
+			break
+		}
+	}
+
+	return ret.Next(i-2), nil
 }
 
 //读取N个字节，并去掉最后的\r\n
-func readRespN(r io.Reader, n int) ([]byte, error) {
+func readRespN(r io.Reader, n int64) ([]byte, error) {
+	var err error
+	var ret []byte
 
+	ret = make([]byte, n)
+	_, err = io.ReadFull(r, ret)
+	if nil!=err {
+		ret = nil
+	}
+	return ret, err
 }
 
 //读取当前行的数字，并去掉最后的\r\n
 func readRespIntLine(r io.Reader) (int64, error) {
-
+	line, err := readRespLine(r)
+	if nil!=err {
+		return 0, err
+	}
+	return strconv.ParseInt(string(line), 10, 64)
 }
