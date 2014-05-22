92e967978166967359a8b9c2e986b3e09b310deb
zhang.mingqiang <zhang.mingqiang@immomo.com>
2014-05-22 19:25:12 +0800

    rewrite all

--- resp.go
+++ resp.go
@@ -1,157 +1,138 @@
-//package resp provides methods to parse and format resp(redis protocal) data
 package resp
 
 import (
 	"io"
-	"fmt"
 	"errors"
 	"bytes"
 	"strconv"
 )
 
 const (
-	T_SimpleString	= '+'
-	T_Error		= '-'
-	T_Integer	= ':'
-	T_BulkString	= '$'
-	T_Array		= '*'
+	T_SimpleString = '+'
+	T_Error	   = '-'
+	T_Integer	   = ':'
+	T_BulkString  = '$'
+	T_Array	   = '*'
 )
 
-type Data struct {
-	T byte
-	str []byte
-	num int64
-	array []*Data
-	isNil bool
+//Command
+//
+//Command 格式：Inline Command 与 Array With BulkString
+type Command struct {
+	//根据惯例，Args[0] 是Name本身
+	Args []string
 }
 
-//string\bulkString
-func (d *Data) String() string {
-	return string(d.str)
-}
-
-func (d *Data) Byte() []byte {
-	return d.str
-}
-
-func (d *Data) Error() string {
-	return string(d.str)
-}
-
-func (d *Data) Integer() int64 {
-	return d.num
-}
-
-func (d *Data) Array() []*Data {
-	return d.array
-}
-
-func (d *Data) IsNil() bool {
-	return d.isNil == true
-}
-
-/*
-valid type: string, []byte, error, int64, []interface{}
-string -> T_SimpleString
-[]byte -> T_BulkString
-error -> T_Error
-int* -> T_Integer
-[]interface{} -> array
-
-*/
-func NewData(val interface{}) (ret *Data, err error) {
-	ret = new(Data)
-	switch val.(type) {
-		case string:
-			ret.T = T_SimpleString
-			ret.str = []byte(val.(string))
-		case error:
-			ret.T = T_Error
-			ret.str = []byte(val.(error).Error())
-		case int64, int8, int16, int32:
-			ret.T = T_Integer
-			ret.num = val.(int64)
-		case []interface{}:
-			ret.T = T_Array
-			ret.array = make([]*Data, len(val.([]interface{})))
-			for index := range val.([]interface{}) {
-				ret.array[index], err = NewData(val.([]interface{}))
-				if nil!=err {
-					goto end
-				}
-			}
-		default:
-			return nil, errors.New("unsupported type")
+//返回Command的名称，如GET\SET
+func (c Command) Name() string {
+	if len(c.Args)==0 {
+		return ""
+	} else {
+		return c.Args[0]
 	}
-end:
-	if nil!=err {
-		ret = nil
-	}
-	return ret, err
 }
 
-func NewSimpleString() {}
-func NewError() {}
-func NewInteger() {}
-func NewArray() {}
-
-//format *Data to []byte
-func FormatData(d *Data) []byte {
-	ret := new(bytes.Buffer)
-	ret.WriteByte(d.T)
-	switch d.T {
-	case T_SimpleString, T_Error:
-		fmt.Fprintf(ret, "%s\r\n", d.str)
-	case T_Integer:
-		fmt.Fprintf(ret, "%d\r\n", d.num)
-	case T_BulkString:
-		fmt.Fprintf(ret, "%d\r\n%s\r\n", len(d.str), string(d.str))
-	case T_Array:
-		fmt.Fprintf(ret, "%d\r\n", len(d.array))
-		for index := range d.array {
-			ret.Write(FormatData(d.array[index]))
-		}
+//以String形式获取Command[index]
+func (c Command) String(index int) (ret string) {
+	if len(c.Args) > index {
+		ret = c.Args[index]
 	}
-	return ret.Bytes()
+	return ret
 }
 
-//read from io.Reader, and parse into *Data
-func ReadData(r io.Reader) (*Data, error) {
+//以int64的形式返回Command.Args[index]
+func (c Command) Integer(index int) (ret int64) {
+	if len(c.Args) > index {
+		ret, _ = strconv.ParseInt(c.Args[index], 10, 64)
+	}
+	return ret
+}
 
-	var buf []byte
-	var err error
+func NewCommand(args ...string) (*Command, error) {
+	if len(args) == 0 {
+		return nil, errors.New("err_new_cmd")
+	}
+	return &Command{Args:args}, nil
+}
 
-	buf = make([]byte, 1)
-	_, err = io.ReadFull(r, buf)
+//从Reader中读取Command
+func ReadCommand(r io.Reader) (*Command, error) {
+	buf := make([]byte, 1)
+	_, err := io.ReadFull(r, buf)
 	if nil != err {
 		return nil, err
 	}
 
-	ret := &Data{}
-	switch buf[0] {
+	if T_Array != buf[0] {
+		return nil, errors.New("Unexpected Command Type")
+	}
+
+	var ret *Data
+	ret = new(Data)
+
+	ret, err = readDataForSpecType(r, buf[0])
+	if nil != err {
+		return nil, err
+	}
+
+	commandArgs := make([]string, len(ret.Array))
+	for index := range ret.Array {
+		if ret.Array[index].T != T_BulkString {
+			return nil, errors.New("Unexpected Command Type")
+		}
+		commandArgs[index] = string(ret.Array[index].String)
+	}
+
+	return NewCommand(commandArgs...)
+}
+
+type Data struct {
+	T byte
+	String []byte
+	Integer int64
+	Array []*Data
+	IsNil bool
+}
+
+func ReadData(r io.Reader) (*Data, error) {
+	buf := make([]byte, 1)
+	_, err := io.ReadFull(r, buf)
+	if nil != err {
+		return nil, err
+	}
+
+	return readDataForSpecType(r, buf[0])
+}
+
+func readDataForSpecType(r io.Reader, t byte) (*Data, error) {
+
+	var err error
+	var ret *Data
+
+	ret = new(Data)
+	switch t {
 		case '+':
 			ret.T = T_SimpleString
-			ret.str, err = readRespLine(r)
+			ret.String, err = readRespLine(r)
 
 		case '-':
 			ret.T = T_Error
-			ret.str, err = readRespLine(r)
+			ret.String, err = readRespLine(r)
 
 		case ':':
 			ret.T = T_Integer
-			ret.num, err = readRespIntLine(r)
+			ret.Integer, err = readRespIntLine(r)
 
 		case '$':
 			var lenBulkString int64
 			lenBulkString, err = readRespIntLine(r)
 
 			ret.T = T_BulkString
-			if -1 == lenBulkString {
-				ret.isNil = true
-			} else {
-				ret.str, err = readRespN(r, lenBulkString)
-				//read the followed \r\n
+			if -1 != lenBulkString {
+				ret.String, err = readRespN(r, lenBulkString)
 				_, err = readRespN(r, 2)
+			} else {
+				ret.IsNil = true
 			}
 
 		case '*':
@@ -160,33 +141,26 @@ func ReadData(r io.Reader) (*Data, error) {
 			lenArray, err = readRespIntLine(r)
 
 			ret.T = T_Array
-			if -1 == lenArray {
-				ret.isNil = true
-			} else if nil==err {
-				ret.array = make([]*Data, lenArray)
-				for i=0; i<lenArray; i++ {
-					ret.array[i], err = ReadData(r)
-				}
-			}
-		default:
-			//Inline Commands
-			tmp, err := readRespLine(r)
 			if nil==err {
-				tmpSlice := bytes.Fields(tmp)
-
-				ret.T = T_Array
-				ret.array = make([]*Data, len(tmpSlice))
-				for index := range tmpSlice {
-					t := &Data{}
-					t.str = tmpSlice[index]
-					t.T = T_SimpleString
-					ret.array[index] = t
+				if -1 != lenArray {
+					ret.Array = make([]*Data, lenArray)
+					for i=0; i<lenArray; i++ {
+						ret.Array[i], err = ReadData(r)
+					}
+				} else {
+					ret.IsNil = true
 				}
 			}
+
+		default: //Maybe you are Inline Command
+			err = errors.New("Unexpected type")
 
 	}
 	return ret, err
 }
+
+
+
 
 //读取当前行，并去掉最后的\r\n
 func readRespLine(r io.Reader) ([]byte, error) {
--- resp_test.go
+++ resp_test.go
@@ -5,88 +5,42 @@ import (
 	"testing"
 )
 
-func TestReadDataString(t *testing.T) {
+type testResult struct {
+	T byte
+	Value interface{}
+}
 
-	stringData := map[string]string {
-		"+Hello\r\n" : "Hello",
-	}
+var validBody map[string]testResult
 
-	for key, target := range stringData {
-		body := bytes.NewBufferString(key)
-		ret, err := ReadData(body)
+func TestValidData(t *testing.T) {
+	for body, result := range validBody {
+		buf := bytes.NewReader([]byte(body))
+		ret, err := ReadData(buf)
+		if nil != err {
+			t.Error(err)
+		}
 
-		if nil!=err || target != ret.String() || ret.T != T_SimpleString {
-			t.Errorf("target:%s result:%s, err:%s", target, ret.String(), err.Error())
+		switch ret.T {
+			case T_SimpleString, T_BulkString:
+				if 0 != bytes.Compare(result.Value.([]byte), ret.String) {
+					t.Error("not eq")
+				}
+			case T_Integer:
+				if result.Value.(int64) != ret.Integer {
+					t.Error("not eq")
+				}
 		}
 	}
 }
 
-func TestReadDataError(t *testing.T) {
-	stringData := map[string]string {
-		"-Redis Server Went Away\r\n" : "Redis Server Went Away",
-	}
-
-	for key, target := range stringData {
-		body := bytes.NewBufferString(key)
-		ret, err := ReadData(body)
-
-		if nil!=err || target != ret.Error() || ret.T != T_Error {
-			t.Errorf("target:%s result:%s, err:%s", target, ret.Error(), err.Error())
-		}
-	}
-
+func BenchmarkReadDataBulkString(b *testing.B) {
 }
 
-
-func TestReadDataInteger(t *testing.T) {
-	stringData := map[string]int64 {
-		":1000\r\n" : 1000,
+func init() {
+	validBody = map[string]testResult {
+		"+OK\r\n" : {T_SimpleString, []byte("OK")},
+		"-Errors\r\n" : {T_Error, []byte("Errors")},
+		":100\r\n" : {T_Integer, int64(100)},
+		"$7\r\nwalu.cc\r\n" : {T_BulkString, []byte("walu.cc")},
 	}
-
-	for key, target := range stringData {
-		body := bytes.NewBufferString(key)
-		ret, err := ReadData(body)
-
-		if nil!=err || target != ret.Integer() || ret.T != T_Integer {
-			t.Errorf("target:%s result:%d, err:%s", target, ret.Integer(), err.Error())
-		}
-	}
-
-}
-
-
-func TestReadDataBulkString(t *testing.T) {
-	stringData := map[string]string {
-		"$6\r\nfoobar\r\n" : "foobar",
-	}
-
-	for key, target := range stringData {
-		body := bytes.NewBufferString(key)
-		ret, err := ReadData(body)
-
-		if nil!=err || target != ret.String() || ret.T != T_BulkString {
-			t.Errorf("target:%s result:%d, err:%s", target, ret.String(), err.Error())
-		}
-	}
-
-}
-
-func TestReadDataArray(t *testing.T) {
-	key := "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n"
-	target := []string{"foo", "bar"}
-
-	body := bytes.NewBufferString(key)
-	ret, err := ReadData(body)
-
-	if nil!=err ||  ret.T != T_Array {
-		t.Error(ret.T)
-	}
-
-	arr := ret.Array()
-	if len(arr) != 2 || arr[0].String() != target[0] || arr[1].String() != target[1] {
-		t.Error(arr[0], arr[1])
-	}
-
-
-
 }
